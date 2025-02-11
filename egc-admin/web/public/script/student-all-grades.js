fee37c525d26faf6c4ded4510637772c3de322a6
SalmaH9 <s.h01063083970@gmail.com>
2025-02-11 08:17:49 +0200

    update1

--- egc-admin/web/public/script/student-all-grades.js
+++ egc-admin/web/public/script/student-all-grades.js
@@ -30,3 +30,37 @@ function displayGrades(grades) {
         tableBody.appendChild(row);
     });
 }
+
+
+
+
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+});
+
+function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+}
+
+const logout = document.getElementById("log_out");
+logout.addEventListener("click", () => {
+    localStorage.clear("userData");
+    location.href = "/home";
+});
+
+
+var userData = JSON.parse(localStorage.getItem("userData"));
+if (userData) {
+    document.getElementsByClassName("profile")[0].getElementsByClassName("name")[0].innerText = userData.name;
+    document.getElementsByClassName("profile")[0].getElementsByClassName("email")[0].innerText = userData.email;
+    if (userData.photo) {
+        document.querySelector(".profile-details img").src = (userData.role == "Student") + '/photo/' + userData.id;
+    }
+}
